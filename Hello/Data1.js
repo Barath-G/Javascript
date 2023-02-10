68532365bd774669ad3d37cbf2206a9d1a2b540c
Barath-G <bharathjovialg@gmail.com>
2023-02-10 19:03:54 +0530

    'JS1'

---
+++ Hello/Data1.js
@@ -0,0 +1,7 @@
+function validateForm() {
+    //alert('Lunch Time')
+    let userName = document.getElementById('uName').value
+    alert(userName)
+    let password = document.getElementById('pwd').value
+    alert(password)
+}
--- Hello/Hello.js
+++ Hello/Hello.js
@@ -1,6 +1,14 @@
-let a = 10;
+/*let a = 10;
 //let b = 10;
 //let c = Math.floor((Math.random()*50+30));
 //a = Number(a);
 //a*=5; // a=a*5
-console.log(a==2 || a==10);
+console.log(a==2 || a==10);*/
+//var name = "B";
+//let msg = "Im learning JS";
+let person = {
+    name: "Nithi",
+    age: 25,
+    address: "Kailasa"
+}
+console.log(person)
