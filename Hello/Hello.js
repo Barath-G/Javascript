8e50761cf1629dfcdbe426f097bf7d8c107f2378
Barath-G <bharathjovialg@gmail.com>
2023-02-10 13:12:37 +0530

    JS

--- Hello/Hello.js
+++ Hello/Hello.js
@@ -1,2 +1,6 @@
-let a = "Bharath Kumar"
-console.log(a);
+let a = 10;
+//let b = 10;
+//let c = Math.floor((Math.random()*50+30));
+//a = Number(a);
+//a*=5; // a=a*5
+console.log(a==2 || a==10);
---
+++ Hello/two.js
@@ -0,0 +1,8 @@
+let enames = ["Anushka", "Hansika", "Bharath", "Samantha", "Nayan"]
+
+enames.map((ename) => { console.log(ename) })
+console.log("&&&&&&&&&&&&&&&&&")
+enames.forEach((ename) => { console.log(ename) })
+/*
+for (ename of enames) {
+    console.log(ename)*/
