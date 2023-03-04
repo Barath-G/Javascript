b0f3af12b197c7262402c0a5146c1dad2542573d
Barath-G <bharathjovialg@gmail.com>
2023-03-04 13:15:44 +0530

    js_arrow

---
+++ Hello/ES6/js_arrow.js
@@ -0,0 +1,9 @@
+/*
+With arrow functions, you don't have to type the function keyword, the return keyword, and the curly brackets.
+
+Arrow functions are not supported in IE11 or earlier.
+*/
+const x = (x, y) => x * y;{
+document.getElementById("demo").innerHTML = x(5, 5);
+}
+console.log(x)
